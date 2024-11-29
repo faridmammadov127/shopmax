28d2d7b097eefcb0cf4610b9520148d85af6b758
faridmammadov127 <faridmammadov03@icloud.com>
2024-11-29 17:55:26 +0400

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Header.module.scss'
+const Header = () => {
+  return (
+    <div className={style.container}>
+        <div className={style.Nav}>
+          <h1>SHOPMAX</h1>
+            
+             </div>
+    </div>
+  )
+}
+
+export default Header
